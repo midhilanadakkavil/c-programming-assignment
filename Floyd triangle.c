2410a6736d70cb3689e14975d41473d18c0d3448
midhilanadakkavil <62107681+midhilanadakkavil@users.noreply.github.com>
2020-03-13 18:13:26 +0530

    Create Floyd triangle.c

---
+++ Floyd triangle.c
@@ -0,0 +1,18 @@
+/* Floyd triangle */
+#include<stdio.h>
+void main()
+{
+	int i,j,r,n=1;
+	printf("Enter the number of rows\n");
+	scanf("%d",&r);
+	printf("Floyd's triangle containing %d rows is\n",r);
+	for(i=0;i<r;i++)
+	{
+	  for(j=0;j<=i;j++)
+	  {
+		  printf("%d ",n);
+		  n++;
+	  }
+	  printf("\n");
+  }
+}
